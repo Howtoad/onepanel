f87367b200090230adcab8086bc334cac8d7d2ee
Kim <howtoad123@gmail.com>
2022-08-23 14:06:59 +0200

    login set up almost done

--- src/modules/Login.js
+++ src/modules/Login.js
@@ -4,21 +4,25 @@ import Input from "../components/Input";
 
 const Login = () => {
   const styles = {
-    loginButtonCss: "text-base text-white bg-opGreen flex leading-7 px-3 py-2",
-    inputCss: "",
-    labelCss: "",
+    loginButtonCss:
+      "text-base text-white bg-opGreen flex leading-7 px-3 py-2 max-w-max mt-9 justify-self-center",
+    inputCss: "ml-2 row-start-1 row-end-2",
+    labelCss: "grid",
+    pCss: "text-base row-start-1 row-end-2 min-w-[72px]",
   };
   return (
-    <div className="flex bg-opGrayBg max-w-max">
+    <div className="grid bg-opGrayBg max-w-max px-5 pt-4 pb-16 gap-y-7">
       <Input
         label={"Username"}
         inputCss={styles.inputCss}
         labelCss={styles.labelCss}
+        pCss={styles.pCss}
       />
       <Input
         label={"Password"}
         inputCss={styles.inputCss}
         labelCss={styles.labelCss}
+        pCss={styles.pCss}
       />
       <Button
         css={styles.loginButtonCss}
