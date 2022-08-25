cb8fe7234c52db4297c27c612cc37bf125afd9a1
Kim <howtoad123@gmail.com>
2022-08-25 12:44:45 +0200

    login version 1 done, added icon and fixed positioning and sizing of things

--- src/modules/Login.js
+++ src/modules/Login.js
@@ -1,6 +1,8 @@
 import { BsFillKeyFill } from "react-icons/bs";
 import Button from "../components/Button";
+import Icon from "../components/Icon";
 import Input from "../components/Input";
+import { BsPerson } from "react-icons/bs";
 
 const Login = () => {
   const styles = {
@@ -9,9 +11,12 @@ const Login = () => {
     inputCss: "ml-2 row-start-1 row-end-2",
     labelCss: "grid",
     pCss: "text-base row-start-1 row-end-2 min-w-[72px]",
+    iconCss: "row-start-1 row-end-2",
+    menuIcon: <BsPerson size={70} className="justify-self-center" />,
   };
   return (
     <div className="grid bg-opGrayBg max-w-max px-5 pt-4 pb-16 gap-y-7">
+      <Icon icon={styles.menuIcon} />
       <Input
         label={"Username"}
         inputCss={styles.inputCss}
