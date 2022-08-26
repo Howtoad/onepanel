3117aa6865adc4d8565f3de8a8f1904238a267fb
Kim <howtoad123@gmail.com>
2022-08-26 11:04:08 +0200

    structuring a bit

--- src/modules/Order.js
+++ src/modules/Order.js
@@ -2,20 +2,30 @@ import Paragraph from "../components/Paragraph";
 
 const Order = ({ price, country, name, address, orderid }) => {
   const styles = {
-    labelCss: "text-red-600",
+    labelCss: "text-sm",
   };
   return (
     <section>
-      <Paragraph text={"Ord.Nr"} css={styles.labelCss} />
-      <Paragraph text={orderid} />
-      <Paragraph text={"price"} css={styles.labelCss} />
-      <Paragraph text={price} />
-      <Paragraph text={"country"} css={styles.labelCss} />
-      <Paragraph text={country} />
-      <Paragraph text={"name"} css={styles.labelCss} />
-      <Paragraph text={name} />
-      <Paragraph text={"address"} css={styles.labelCss} />
-      <Paragraph text={address} />
+      <div>
+        <Paragraph text={"Ord.Nr"} css={styles.labelCss} />
+        <Paragraph text={orderid} />
+      </div>
+      <div>
+        <Paragraph text={"price"} css={styles.labelCss} />
+        <Paragraph text={price} />
+      </div>
+      <div>
+        <Paragraph text={"country"} css={styles.labelCss} />
+        <Paragraph text={country} />
+      </div>
+      <div>
+        <Paragraph text={"name"} css={styles.labelCss} />
+        <Paragraph text={name} />
+      </div>
+      <div>
+        <Paragraph text={"address"} css={styles.labelCss} />
+        <Paragraph text={address} />
+      </div>
     </section>
   );
 };
