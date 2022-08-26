4674529046899cfeaac0c7bf88b7d02ae3f140fa
Alexander-Gronninger <93522244+Alexander-Gronninger@users.noreply.github.com>
2022-08-26 09:30:41 +0200

    fix card css and fix heading element checking

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -2,10 +2,11 @@ import useFetch from "../customHooks/useFetch";
 import ProductCard from "../templates/ProductCard";
 
 const styles = {
-  card: "bg-opGrayBg p-1.5 border-solid border-black border w-44",
-  image: "m-auto",
-  heading: "text-xl font-semibold m-auto max-w-max",
-  paragraph: "m-auto max-w-max text-base overflow-hidden leading-5 h-10",
+  card: "bg-opGrayBg p-1.5 border-solid border-black border w-44 flex flex-col",
+  image: "m-auto mb-auto",
+  heading:
+    "text-xl font-semibold mx-auto max-w-max overflow-hidden leading-7 h-14",
+  paragraph: "mx-auto max-w-max text-base overflow-hidden leading-5 h-10",
 };
 
 const ProductList = () => {
@@ -15,7 +16,7 @@ const ProductList = () => {
 
   return (
     <>
-      <section className="grid grid-cols-2 p-1.5 gap-y-2 gap-x-1">
+      <section className="grid grid-cols-2 p-1.5 gap-y-2 gap-x-1 mb-20">
         {data[0] &&
           data.map((product) => {
             return (
