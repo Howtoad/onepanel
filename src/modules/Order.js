24798c64705724e8359359123a6f0c8d46dd7027
Kim <howtoad123@gmail.com>
2022-08-27 20:10:15 +0200

    some set up done, got annoyed and gonna do something else

--- src/modules/Order.js
+++ src/modules/Order.js
@@ -3,29 +3,30 @@ import Paragraph from "../components/Paragraph";
 const Order = ({ price, country, name, address, orderid }) => {
   const styles = {
     labelCss: "text-sm",
+    valueCss: "text-sm bg-white max-w-max",
   };
   return (
-    <section>
-      <div>
-        <Paragraph text={"Ord.Nr"} css={styles.labelCss} />
-        <Paragraph text={orderid} />
-      </div>
-      <div>
-        <Paragraph text={"price"} css={styles.labelCss} />
-        <Paragraph text={price} />
-      </div>
-      <div>
-        <Paragraph text={"country"} css={styles.labelCss} />
-        <Paragraph text={country} />
-      </div>
-      <div>
-        <Paragraph text={"name"} css={styles.labelCss} />
-        <Paragraph text={name} />
-      </div>
-      <div>
-        <Paragraph text={"address"} css={styles.labelCss} />
-        <Paragraph text={address} />
-      </div>
+    <section className="grid bg-opGrayBg grid-rows-2 grid-cols-2">
+      {/* <div className="flex"> */}
+      <Paragraph text={"Ord.Nr"} css={styles.labelCss} />
+      <Paragraph text={orderid} css={styles.valueCss} />
+      {/* </div> */}
+      {/* <div className="flex"> */}
+      <Paragraph text={"price"} css={styles.labelCss} />
+      <Paragraph text={price} css={styles.valueCss} />
+      {/* </div> */}
+      {/* <div className="flex"> */}
+      <Paragraph text={"country"} css={styles.labelCss} />
+      <Paragraph text={country} css={styles.valueCss} />
+      {/* // </div> */}
+      {/* <div className="flex"> */}
+      <Paragraph text={"name"} css={styles.labelCss} />
+      <Paragraph text={name} css={styles.valueCss} />
+      {/* // </div> */}
+      {/* <div className="flex"> */}
+      <Paragraph text={"address"} css={styles.labelCss} />
+      <Paragraph text={address} css={styles.valueCss} />
+      {/* // </div> */}
     </section>
   );
 };
