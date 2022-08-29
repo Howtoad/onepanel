0744ae796b90d55df3c8fc2d3a55a3d5418f26ef
Kim <howtoad123@gmail.com>
2022-08-29 12:48:16 +0200

    Orders edition 1 is done, with styling and upset of components

--- src/modules/Order.js
+++ src/modules/Order.js
@@ -1,32 +1,33 @@
 import Paragraph from "../components/Paragraph";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 const Order = ({ price, country, name, address, orderid }) => {
   const styles = {
-    labelCss: "text-sm",
-    valueCss: "text-sm bg-white max-w-max",
+    labelCss: "text-sm leading-4 capitalize px-1",
+    valueCss: "text-sm bg-white max-w-max leading-4 capitalize px-1",
   };
   return (
-    <section className="grid bg-opGrayBg grid-rows-2 grid-cols-2">
-      {/* <div className="flex"> */}
-      <Paragraph text={"Ord.Nr"} css={styles.labelCss} />
-      <Paragraph text={orderid} css={styles.valueCss} />
-      {/* </div> */}
-      {/* <div className="flex"> */}
-      <Paragraph text={"price"} css={styles.labelCss} />
-      <Paragraph text={price} css={styles.valueCss} />
-      {/* </div> */}
-      {/* <div className="flex"> */}
-      <Paragraph text={"country"} css={styles.labelCss} />
-      <Paragraph text={country} css={styles.valueCss} />
-      {/* // </div> */}
-      {/* <div className="flex"> */}
-      <Paragraph text={"name"} css={styles.labelCss} />
-      <Paragraph text={name} css={styles.valueCss} />
-      {/* // </div> */}
-      {/* <div className="flex"> */}
-      <Paragraph text={"address"} css={styles.labelCss} />
-      <Paragraph text={address} css={styles.valueCss} />
-      {/* // </div> */}
+    <section className="grid bg-opGrayBg ">
+      <div className="flex mb-1 mt-1">
+        <Paragraph text={"Ord.Nr"} css={styles.labelCss} />
+        <Paragraph text={orderid} css={styles.valueCss} />
+
+        <Paragraph text={"price"} css={styles.labelCss} />
+        <Paragraph text={price} css={styles.valueCss} />
+
+        <Paragraph text={"country"} css={styles.labelCss} />
+        <Paragraph text={country} css={styles.valueCss} />
+      </div>
+      <div className="flex ">
+        <Paragraph text={"name"} css={styles.labelCss} />
+        <Paragraph text={name} css={styles.valueCss} />
+
+        <Paragraph text={"address"} css={styles.labelCss} />
+        <Paragraph text={address} css={styles.valueCss} />
+      </div>
+      <div className="justify-self-center">
+        <MdKeyboardArrowDown size={32} />
+      </div>
     </section>
   );
 };
